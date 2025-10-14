8c07712d83ed35f9ab40016debee655f9e1afcf1
emanu <emanu@MRKUKONOTEBOOK3>
2025-10-14 20:57:49 +0200

    Add project files.

---
+++ MarkdownTrayApp/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace MarkdownTrayApp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Start minimized to tray if desired
+            // Uncomment to hide window on startup
+            // MainWindow.Hide();
+        }
+    }
+}
